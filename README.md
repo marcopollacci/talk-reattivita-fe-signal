2e9d8e6725fb60218ad663c666238300691a3463
Marco Pollacci <pollacci@gmail.com>
2025-02-12 22:01:32 +0100

    feat(main): :tada: fork from skeleton

---
+++ README.md
@@ -0,0 +1,25 @@
+# Some beautiful title
+
+Slides for what?.
+
+Originally conceived for the what?.
+
+Built on top of [P-Slides](https://github.com/MaxArt2501/p-slides)!
+
+# Url Slide
+
+## Building
+
+Node.js >= 10.x is required.
+
+To start the presentation in development mode:
+
+1. Clone the repository;
+2. `npm i`
+3. `npm start`
+
+Or `npm run build` to just build it, then serve `/public` with your HTTP server of choice.
+
+## Tests
+
+![meme-there-is-no-test-production-only-there-is.jpg](https://i.postimg.cc/9Q477w17/meme-there-is-no-test-production-only-there-is.jpg)
---
+++ serve.js
@@ -0,0 +1,42 @@
+import bSync from 'browser-sync';
+import { relative } from 'node:path';
+import { buildJavaScript, buildSlides, buildStyle, copyAssets } from './build.js';
+
+const server = bSync.create();
+
+const baseDir = './public';
+const cssDir = `${baseDir}/css`;
+
+const startPath = process.argv[2] || '';
+
+server.init({
+  ghostMode: false,
+  startPath,
+  server: { baseDir }
+});
+
+server.watch('src/**/*.html', {}, (event, file) => {
+  console.log(`src/**/*.html -> ${event}: ${file}`);
+  buildSlides();
+  server.reload();
+});
+
+server.watch('src/js/**/*.js', {}, (event, file) => {
+  console.log(`src/js/**/*.js -> ${event}: ${file}`);
+  if (!['add', 'change'].includes(event)) return;
+  buildJavaScript([file]);
+  server.reload();
+});
+
+server.watch('src/styles/**/*.scss', {}, (event, file) => {
+  console.log(`src/styles/**/*.scss -> ${event}: ${file}`);
+  const modified = buildStyle();
+  server.reload(modified.map(path => relative(cssDir, path)));
+});
+
+server.watch('static/**/*', {}, (event, file) => {
+  console.log(`static/**/* -> ${event}: ${file}`);
+  if (!['add', 'change'].includes(event)) return;
+  const modified = copyAssets([file]);
+  server.reload(modified.map(path => relative(baseDir, path)));
+});
